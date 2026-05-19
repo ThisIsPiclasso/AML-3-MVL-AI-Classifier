d81ea1c7c33bbbb7a35ebae71eaa9d4f906f76fe
ThisIsPiclasso <github.buckwheat412@passfwd.com>
2026-05-19 13:44:33 +0000

    add intial test model configuration for correct formatting

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # This is a sample Python script.
+from MVL_AI_Classifier.features.aps_pipeline import AzimuthalPowerSpectrumPreprocessor
+from MVL_AI_Classifier.constants import DEFAULT_N_BINS
+
+MODEL_CONFIGURATION = {
+    "aps": {
+        "preprocessor": AzimuthalPowerSpectrumPreprocessor(),
+        "model_type": "cnn",  # or mlp
+        "input_shape": (DEFAULT_N_BINS,),
+    }
+}
+
 
 def hello_world():
     return "Hello, World!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     hello_world()
