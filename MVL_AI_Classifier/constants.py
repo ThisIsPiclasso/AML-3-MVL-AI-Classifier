3c332e81bb3b5995992bb7d89c8372f0790eaf57
Lara Anna van den Broek <l.a.vandenbroek27@gmail.com>
2026-05-22 09:55:38 +0200

    Add default parameters for noise residuals

--- MVL_AI_Classifier/constants.py
+++ MVL_AI_Classifier/constants.py
@@ -22,5 +22,9 @@ DEFAULT_N_BINS = 64
 DEFAULT_N_LEVELS = 32
 
 #dct default parameters
-DEFAULT_LOG_COMPRESS_DISPERSION = True
-DEFAULT_SPARSITY_THRESHOLD = 0.10
+DEFAULT_LOG_COMPRESS_DISPERSION = True #log compression for MAD statistic
+DEFAULT_SPARSITY_THRESHOLD = 0.10 #sparsity threshold for dct statistic
+
+#noise residuals default parameters
+DEFAULT_SIGMA = 1.0 #sigma value for gaussian filter
+DEFAULT_WINDOW_SIZE = 16 
